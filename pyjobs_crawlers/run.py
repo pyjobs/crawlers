2813c5d089a7974b180796e79b93981dce6af38d
Bastien Sevajol <bastien.sevajol@algoo.fr>
2016-01-14 12:36:16 +0100

    Separe lock and crawl code

--- pyjobs_crawlers/run.py
+++ pyjobs_crawlers/run.py
@@ -31,39 +31,24 @@ def crawl(site_file_name):
     :param site_file_name: python file considered as JobSpider
     :return:
     """
+    cmdname = 'runspider'
+    settings = get_project_settings()
+    cmds = _get_commands_dict(settings=settings, inproject=True)
 
-    # Lock to prevent simultaneous crawnling process
-    lock_name = "pyjobs_crawl_%s" % slugify(basename(site_file_name))
-    lock = fasteners.InterProcessLock('/tmp/%s' % lock_name)
-    lock_gotten = lock.acquire(blocking=False)
+    parser = optparse.OptionParser(formatter=optparse.TitledHelpFormatter(),
+                                   conflict_handler='resolve')
+    cmd = cmds[cmdname]
+    parser.usage = "scrapy %s %s" % (cmdname, cmd.syntax())
+    parser.description = cmd.long_desc()
+    settings.setdict(cmd.default_settings, priority='command')
+    cmd.settings = settings
+    cmd.add_options(parser)
+    opts, args = parser.parse_args(args=[site_file_name])
 
-    try:
-        if not lock_gotten:
-            print("Crawl of %s already running" % site_file_name)
-            pass  # TODO - B.S. - 20160114: On le dit ailleurs (log) que le process est déjà en cours ?
+    cmd.process_options(args, opts)
+    cmd.crawler_process = CrawlerProcess(settings)
 
-        cmdname = 'runspider'
-        settings = get_project_settings()
-        cmds = _get_commands_dict(settings=settings, inproject=True)
-
-        parser = optparse.OptionParser(formatter=optparse.TitledHelpFormatter(),
-                                       conflict_handler='resolve')
-        cmd = cmds[cmdname]
-        parser.usage = "scrapy %s %s" % (cmdname, cmd.syntax())
-        parser.description = cmd.long_desc()
-        settings.setdict(cmd.default_settings, priority='command')
-        cmd.settings = settings
-        cmd.add_options(parser)
-        opts, args = parser.parse_args(args=[site_file_name])
-
-        cmd.process_options(args, opts)
-        cmd.crawler_process = CrawlerProcess(settings)
-
-        _run_command(cmd, args, opts)
-
-    finally:
-        if lock_gotten:
-            lock.release()
+    _run_command(cmd, args, opts)
 
 
 def start_crawlers(processes=2):
@@ -79,10 +64,26 @@ def start_crawlers(processes=2):
     # split list in x list of processes count elements
     spiders_files_chunks = [spiders_files[x:x+processes] for x in range(0, len(spiders_files), processes)]
 
+    def start_crawl_process(site_file_name):
+        # Lock to prevent simultaneous crawnling process
+        lock_name = "pyjobs_crawl_%s" % slugify(basename(site_file_name))
+        lock = fasteners.InterProcessLock('/tmp/%s' % lock_name)
+        lock_gotten = lock.acquire(blocking=False)
+
+        try:
+            if lock_gotten:
+                crawl(site_file_name)
+            else:
+                print("Crawl of %s already running" % site_file_name)
+                # TODO - B.S. - 20160114: On le dit ailleurs (log) que le process est déjà en cours ?
+        finally:
+            if lock_gotten:
+                lock.release()
+
     # Start one cycle of processes by chunk
     for spiders_files_chunk in spiders_files_chunks:
         p = Pool(len(spiders_files_chunk))
-        p.map(crawl, spiders_files_chunk)
+        p.map(start_crawl_process, spiders_files_chunk)
 
 
 if __name__ == '__main__':
