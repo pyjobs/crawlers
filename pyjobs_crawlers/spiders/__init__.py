cadb8cf3d946df73458bef96a29a82eba32b59ad
Bastien Sevajol <bastien.sevajol@algoo.fr>
2016-01-15 16:30:03 +0100

    Refactoring of crawler

--- pyjobs_crawlers/spiders/__init__.py
+++ pyjobs_crawlers/spiders/__init__.py
@@ -7,8 +7,10 @@
 
 import urlparse
 from scrapy.utils.response import get_base_url
-
 from scrapy import Spider, Item, Field, Request
+import datetime
+from pyjobs_crawlers.items import JobItem
+
 
 class Tag(object):
     def __init__(self, tag, iteration_nb=1):
@@ -16,6 +18,12 @@ class Tag(object):
         self.weight = iteration_nb
 
 class JobSpider(Spider):
+    """
+    TODO - B.S. - 20160115: Dans _get_jobs_container (et autres) on récupère le [0].
+                            Tester si il y est et lever erreur (log + ?) si il n'y est pas.
+                            De manière générale, tester et alerter partout où on va utiliser
+                            les crawl parameters
+    """
 
     """Beginning of work"""
     ACTION_START = 'START'
@@ -35,17 +43,36 @@ class JobSpider(Spider):
     """Marker (last crawled point) found: stop crawling"""
     ACTION_MARKER_FOUND = 'MARKER_FOUND'
 
-    def __init__(self, *args, **kwargs):
-        super(JobSpider, self).__init__(*args, **kwargs)
-        self._connector = None
-
-    start_urls = []  # To be overwritten
-    name = 'no-name'  # To be overwritten
-
     COMMON_TAGS = [
         u'cdi', u'cdd', u'télétravail', u'stage', u'freelance', u'mysql'
         u'postgresql', u'django'
     ]
+
+    start_urls = []  # To be overwritten
+    name = 'no-name'  # To be overwritten
+
+    crawl_parameters = {
+        'job_list_xpath': None,
+        'job_list_element_url_xpath': None,
+        'job_list_next_page_xpath': None,
+        'job_node_xpath': '//body',
+        'job_title_xpath': './h1/text()',
+        'job_publication_date_xpath': None,
+        'job_company_name_xpath': None,
+        'job_company_url_xpath': None,
+        'job_address_xpath': None,
+        'job_description_xpath': None,
+        'job_tags_xpath': None,
+    }
+
+    def __init__(self, *args, **kwargs):
+        super(JobSpider, self).__init__(*args, **kwargs)
+        self._connector = None
+
+    def _get_parameter(self, parameter_name):
+        if not self.crawl_parameters[parameter_name]:
+            raise Exception("Crawl Parameter \"%s\" is not set")
+        return self.crawl_parameters[parameter_name]
 
     def _set_crawler(self, crawler):
         super(JobSpider, self)._set_crawler(crawler)
@@ -62,14 +89,107 @@ class JobSpider(Spider):
 
     def parse(self, response):
         self.get_connector().log(self.name, self.ACTION_START)
-        print 'start crawling...'
         return self.parse_job_list_page(response)
 
     def parse_job_list_page(self, response):
-        raise NotImplementedError('Must be overwritten')
+        self.get_connector().log(self.name, self.ACTION_CRAWL_LIST, response.url)
+
+        for job in self._get_jobs_nodes(response):
+            # first we check url. If the job exists, then skip crawling
+            # (it means that the page has already been crawled
+            url = self._get_jobs_node_url(job)
+
+            if self.get_connector().job_exist(url):
+                self.get_connector().log(self.name, self.ACTION_MARKER_FOUND, url)
+                return
+
+            yield Request(url, self.parse_job_page)
+
+        next_page_url = self._get_next_page_url(response)
+        yield Request(url=next_page_url)
 
     def parse_job_page(self, response):
-        raise NotImplementedError('Must be overwritten')
+        self.get_connector().log(self.name, self.ACTION_CRAWL_JOB, response.url)
+
+        item = JobItem()
+
+        job_container = self._get_job_container(response)
+        item['source'] = self.name
+        item['initial_crawl_datetime'] = datetime.datetime.now()
+        item['url'] = response.url
+        item['title'] = self._get_job_title(job_container)
+        item['publication_datetime'] = self._get_job_publication_date(job_container)
+        item['company'] = self._get_job_company_name(job_container)
+        item['company_url'] = self._get_job_company_url(job_container)
+        item['address'] = self._get_job_address(job_container)
+        item['description'] = self._get_job_description(job_container)
+
+        job_tags_xpath = self._get_parameter('job_tags_xpath')
+        item['tags'] = self.extract_tags(self._extract(job_container, job_tags_xpath))
+
+        item['status'] = JobItem.CrawlStatus.COMPLETED
+
+        yield item
+
+    def _get_jobs_nodes(self, response):
+        job_list_xpath = self._get_parameter('job_list_xpath')
+        return response.xpath(job_list_xpath)
+
+    def _get_jobs_node_url(self, jobs_node):
+        job_list_element_url_xpath = self._get_parameter('job_list_element_url_xpath')
+        return self._extract(jobs_node, job_list_element_url_xpath)
+
+    def _get_next_page_url(self, response):
+        job_list_next_page_xpath = self._get_parameter('job_list_next_page_xpath')
+        return self._extract(response, job_list_next_page_xpath)
+
+    def _get_job_container(self, response):
+        job_node_xpath = self._get_parameter('job_node_xpath')
+        return response.xpath('//div[@id="content"]')[0]
+
+    def _get_job_title(self, job_container):
+        job_title_xpath = self._get_parameter('job_title_xpath')
+        return self._extract(job_container, job_title_xpath)
+
+    def _get_job_publication_date(self, job_container):
+        return datetime.datetime.now()
+
+    def _get_job_company_name(self, job_container):
+        job_company_name_xpath = self._get_parameter('job_company_name_xpath')
+        return self._extract(job_container, job_company_name_xpath)
+
+    def _get_job_company_url(self, job_container):
+        job_company_url_xpath = self._get_parameter('job_company_url_xpath')
+        return self._extract(job_container, job_company_url_xpath)
+
+    def _get_job_address(self, job_container):
+        job_address_xpath = self._get_parameter('job_address_xpath')
+        return self._extract(job_container, job_address_xpath)
+
+    def _get_job_description(self, job_container):
+        job_description_xpath = self._get_parameter('job_description_xpath')
+        return self._extract(job_container, job_description_xpath)
+
+    def _extract(self, container, selector):
+        """
+
+        :param container:
+        :param selector: xpath or tuple
+        :return: Extracted text value OR None
+        """
+        if isinstance(selector, (list, tuple)):
+            for selector_option in selector:
+                extracted_value = self._extract(container, selector_option)
+                if extracted_value:
+                    return extracted_value
+
+        extract = container.xpath(selector).extract_first()
+
+        if not extract:
+            return None
+
+        # The join of splited text remove \n \t etc ...
+        return ' '.join(extract.split())
 
     def _extract_common_tags(self, html_content):
         for tag in self.COMMON_TAGS:
--- pyjobs_crawlers/spiders/afpy.py
+++ pyjobs_crawlers/spiders/afpy.py
@@ -10,26 +10,59 @@ class AfpyJobSpider(JobSpider):
     name = 'afpy'
     start_urls = ['http://www.afpy.org/jobs']
 
-    def parse_job_list_page(self, response):
-        self.get_connector().log(self.name, self.ACTION_CRAWL_LIST, response.url)
+    crawl_parameters = {
+        'job_list_xpath': '//div[@class="jobitem"]',
+        'job_list_element_url_xpath': './a/@href',
+        'job_list_next_page_xpath': '//div[@class="listingBar"]/span[@class="next"]/a/@href',
+        'job_node_xpath': '//div[@id="content"]',
+        'job_title_xpath': './h1[@id="parent-fieldname-title"]/text()',
+        'job_publication_date_xpath': './div[@id="content-core"]/div[@id="content-core"]/div[@class="discreet"]/text()',
+        'job_company_name_xpath': ('.//h4/a/text()', './/h4/text()'),
+        'job_company_url_xpath': './div[@id="content-core"]/div[@id="content-core"]/h4/a/@href',
+        'job_address_xpath': './/h4[1]/following-sibling::div[@class="row"]/text()',
+        'job_description_xpath': './div[@id="content-core"]/div[@id="content-core"]',
+        'job_tags_xpath': './div[@id="content-core"]/div[@id="content-core"]'
+    }
 
-        for job in response.xpath('//div[@class="jobitem"]'):
-            # first we check url. If the job exists, then skip crawling
-            # (it means that the page has already been crawled
-            url_xpath = './a/@href'
-            url = job.xpath(url_xpath)[0].extract()
+    # def parse_job_list_page(self, response):
+    #     self.get_connector().log(self.name, self.ACTION_CRAWL_LIST, response.url)
+    #
+    #     for job in response.xpath('//div[@class="jobitem"]'):
+    #         # first we check url. If the job exists, then skip crawling
+    #         # (it means that the page has already been crawled
+    #         url_xpath = './a/@href'
+    #         url = job.xpath(url_xpath)[0].extract()
+    #
+    #         if self.get_connector().job_exist(url):
+    #             self.get_connector().log(self.name, self.ACTION_MARKER_FOUND, url)
+    #             return
+    #
+    #         yield Request(url, self.parse_job_page)
+    #
+    #     next_page_url_xpath = '//div[@class="listingBar"]/span[@class="next"]/a/@href'
+    #     next_page_url = response.xpath(next_page_url_xpath)[0].extract()
+    #     yield Request(url=next_page_url)
 
-            if self.get_connector().job_exist(url):
-                self.get_connector().log(self.name, self.ACTION_MARKER_FOUND, url)
-                return
 
-            yield Request(url, self.parse_job_page)
+    def _get_job_title(self, job_container):
+        job_title_xpath = self._get_parameter('job_title_xpath')
+        title_lines = job_container.xpath(job_title_xpath)[0].extract().strip().splitlines()
+        title = u''
+        for line in title_lines:
+            if line.strip() != u'':
+                title += line
+        return title
 
-        next_page_url_xpath = '//div[@class="listingBar"]/span[@class="next"]/a/@href'
-        next_page_url = response.xpath(next_page_url_xpath)[0].extract()
-        yield Request(url=next_page_url)
+    def _get_job_publication_date(self, job_container):
+        job_publication_date_xpath = self._get_parameter('job_publication_date_xpath')
+        try:
+            date_text = job_container.xpath(job_publication_date_xpath)[0].extract()\
+                .strip().splitlines()[0].strip().replace(u'Créé le ', '')
+            return datetime.strptime(date_text, '%d/%m/%Y %H:%M')
+        except Exception:
+            return super(AfpyJobSpider, self)._get_job_publication_date(job_container)
 
-    def parse_job_page(self, response):
+    def parse_job_page__(self, response):
         self.get_connector().log(self.name, self.ACTION_CRAWL_JOB, response.url)
 
         job_node = response.xpath('//div[@id="content"]')[0]
