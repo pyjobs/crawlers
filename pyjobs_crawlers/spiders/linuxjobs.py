f1d076788b9cf968ad0b27a37e91e415379c6065
Bastien Sevajol <bastien.sevajol@algoo.fr>
2016-01-18 12:13:36 +0100

    linuxjobs refactoring

--- pyjobs_crawlers/spiders/linuxjobs.py
+++ pyjobs_crawlers/spiders/linuxjobs.py
@@ -5,12 +5,27 @@ from datetime import datetime
 from scrapy import Request
 from pyjobs_crawlers.spiders import JobSpider
 from pyjobs_crawlers.items import JobItem
+from pyjobs_crawlers.spiders import NotCrawlable
 
 
 class LolixJobSpider(JobSpider):
-
     name = 'linuxjobs'
     start_urls = ['https://www.linuxjobs.fr/search/python']
+
+    _crawl_parameters = {
+        'jobs_css': '.list-group',
+        'jobs_job_css': ' .list-group-item',
+        'jobs_job_element_url_xpath': './@href',
+        'job_node_css': '.container',
+        'job_title_css': 'div.container div.row:nth-child(2) h2::text',
+        'job_publication_date_css': 'small.muted::text',
+        'job_company_name_css': 'div.container div.row:nth-child(2) .col-md-9 h4:nth-child(3)::text',
+        'job_company_url_css': ('div.container div.row:nth-child(2) .col-md-9 h4:nth-child(5) a::text',
+                                'div.container div.row:nth-child(2) .col-md-9 h4:nth-child(5)::text'),
+        'job_address_css': 'div.container div.row:nth-child(2) .col-md-9 h4:nth-child(4)::text',
+        'job_description_css': 'div.container div.row:nth-child(4) div.col-md-9',
+        'job_tags_css': 'div.container div.row:nth-child(4) div.col-md-9'
+    }
 
     def _month_french_to_english(self, datetime_str):
         months = {
@@ -34,73 +49,20 @@ class LolixJobSpider(JobSpider):
             out = out.replace(key, value)
         return out
 
-    def parse_job_list_page(self, response):
-        self.get_connector().log(self.name, self.ACTION_CRAWL_LIST, response.url)
+    def _get_jobs_node_url(self, jobs_node):
+        if len(jobs_node.css('h4')) < 1: # If no h4, then, this is not a job
+            raise NotCrawlable()
+        return super(LolixJobSpider, self)._get_jobs_node_url(jobs_node)
 
-        i = 0
-        for group in response.css('div.list-group'):
-            goto_next_group = False
 
-            for job_row in group.css('a.list-group-item'):
-                if len(job_row.css('h4')) <= 0:  # If no H4, then, this is not a job
-                    goto_next_group = True
+    def _get_job_publication_date(self, job_container):
+        publication_datetime_str = self._extract_first(job_container, 'job_publication_date')
+        publication_datetime_str = publication_datetime_str.replace(u'Ajout\xe9e le', '')
+        publication_datetime_str_english = self._month_french_to_english(publication_datetime_str)
+        return datetime.strptime(publication_datetime_str_english, '%d %B %Y')
 
-                if not goto_next_group:
-                    print 'ITERATION #{}'.format(i)
-                    url = job_row.xpath('./@href')[0].extract()
-
-                    if self.get_connector().job_exist(url):
-                        self.get_connector().log(self.name, self.ACTION_MARKER_FOUND, url)
-                        return  # job already found; stop scrapying
-
-                    job = JobItem()
-                    job['status'] = JobItem.CrawlStatus.PREFILLED
-                    job['url'] = url
-                    job['source'] = self.name
-                    job['title'] = job_row.css('h4 > span.job-title::text')[0].extract()
-                    job['company'] = job_row.css('h4 > span.job-company::text')[0].extract()
-
-                    publication_datetime_str = job_row.css('h4 > span.pull-right::text')[0].extract()
-                    publication_datetime_str_english = self._month_french_to_english(publication_datetime_str)
-                    print ('PUB DATE IS', publication_datetime_str_english)
-                    publication_datetime = datetime.strptime(publication_datetime_str_english, '%d %B %Y')
-                    job['publication_datetime'] = publication_datetime
-                    job['initial_crawl_datetime'] = datetime.now()
-
-                    job['address'] = ''
-                    job['company_url'] = ''
-
-                    print 'JOB IS', job
-                    job_detail_request = Request(job['url'],
-                                                 callback=self.parse_job_page)
-                    job_detail_request.meta['item'] = job
-                    yield job_detail_request
-
-        # TODO - Activate other pages
-        # next_page_url_xpath = '//a[@class="T3" and text()="Page suivante ->"]/@href'
-        # next_page_url = self._build_url(
-        #     response=response,
-        #     path=response.xpath(next_page_url_xpath)[0].extract()
-        # )
-        #
-        # yield Request(url=next_page_url)
-
-    def parse_job_page(self, response):
-        self.get_connector().log(self.name, self.ACTION_CRAWL_JOB, response.url)
-
-        job = response.meta['item']  # prefilled item
-
-        job_header = response.css('div.row')[0].css('div.col-md-9')
-        job_address = job_header.xpath('.//h4[2]/text()')[0].extract()
-        job['address'] = re.sub(r'\([^)]*\)', '', job_address).strip()  # address is like Paris (programmeurs)
-        job['company_url'] = job_header.xpath('.//h4[3]/a/@href')[0].extract()
-
-        job_description = job_header = response.css('div.row')[2].css('div.col-md-9').extract()
-        job['description'] = job_description
-        job['status'] = JobItem.CrawlStatus.COMPLETED
-
-        print 'JOB IS NOW ===> ', job
-
-        if JobItem.CrawlStatus.COMPLETED == job['status']:
-            yield job
-
+    def _get_job_address(self, job_container):
+        address = super(LolixJobSpider, self)._get_job_address(job_container)
+        if address:
+            return re.sub(r'\([^)]*\)', '', address).strip() #  address is like Paris (programmeurs)
+        return None
