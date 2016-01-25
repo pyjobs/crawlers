334bfb8d84600551e57f08cf36ed164a1fe0afad
Bastien Sevajol <bastien.sevajol@algoo.fr>
2016-01-25 10:59:51 +0100

    result code in run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -4,4 +4,7 @@ import unittest
 from tests.functional.spiders.afpy import TestAfpySpider
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestAfpySpider)
-unittest.TextTestRunner(verbosity=2).run(suite)
+test_result = unittest.TextTestRunner(verbosity=2).run(suite)
+
+if test_result.failures or test_result.errors:
+    exit(1)
