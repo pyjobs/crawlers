9790692d4f3167521ee68e9ff1bfe40077f97df3
Bastien Sevajol <bastien.sevajol@algoo.fr>
2016-01-25 10:55:16 +0100

    Add comments

--- pyjobs_crawlers/test.py
+++ pyjobs_crawlers/test.py
@@ -15,6 +15,8 @@ class FunctionalTest(unittest.TestCase):
 
 
 class SpiderTest(FunctionalTest):
+
+    # These class attributes must be implemented. TODO - B.S. 20160125: Encapsutalte them ?
     _spider_class = None
     _expected_jobs = None
     _test_dir = ''
@@ -23,6 +25,13 @@ class SpiderTest(FunctionalTest):
     _replace = ''
 
     def _crawl(self, start_file_path, fake_url, items=[]):
+        """
+
+        :param start_file_path: file path of start file
+        :param fake_url: The fake url for Request
+        :param items: List of jobs item to use as "job database". Default is empty list
+        :return: list of job items
+        """
         connector = SpiderTestConnector(items)
 
         request = Request(url=fake_url)
@@ -37,7 +46,9 @@ class SpiderTest(FunctionalTest):
         return list(self._parse_spider_requests(spider.parse(start_response)))
 
     def _get_prepared_spider(self):
-
+        """
+        :return: Spider class override to manipulate founded urls
+        """
         replace = self._replace
         dump_dir = self._dump_dir
 
@@ -53,6 +64,10 @@ class SpiderTest(FunctionalTest):
         return OverridedSpider
 
     def _parse_spider_requests(self, spider_response):
+        """
+        :param spider_response: return of parse spider method
+        :return: job item generator
+        """
         for response_item in spider_response:
             if isinstance(response_item, Request):
                 request = response_item
@@ -71,9 +86,9 @@ class SpiderTest(FunctionalTest):
 
     def _get_result_html_file(self, html_file, items=[]):
         """
-        :param html_file:
-        :param results:
-        :return:
+        :type items: list
+        :param html_file: html file to start crawl
+        :return: list of item jobs
         """
         return self._crawl(
                 start_file_path=os.path.join(self._dump_dir, html_file),
@@ -82,6 +97,11 @@ class SpiderTest(FunctionalTest):
         )
 
     def _get_job_from_expected(self, job_title):
+        """
+        Return a job from expected job, with title
+        :param job_title: title of expected job
+        :return: job dict
+        """
         for job in self._expected_jobs:
             if job['title'] == job_title:
                 return job
@@ -89,12 +109,24 @@ class SpiderTest(FunctionalTest):
 
     @staticmethod
     def _get_job_from_result_with_title(result, job_title):
+        """
+        Extract job from result with title
+        :param result: return of parse spider method
+        :param job_title: title of wanted job
+        :return: job item
+        """
         for job in result:
             if job['title'] == job_title:
                 return job
         raise NotFound("Job \"%s\" not found in results")
 
     def _result_contains_jobs(self, result, expected_jobs_titles):
+        """
+        Check results jobs with expected jobs
+        :param result: return of parse spider method
+        :param expected_jobs_titles: list of jobs title
+        :return:
+        """
         for expected_job_title in expected_jobs_titles:
             expected_job = self._get_job_from_expected(expected_job_title)
             try:
@@ -104,6 +136,12 @@ class SpiderTest(FunctionalTest):
             self._compare_jobs(expected_job, result_job)
 
     def _compare_jobs(self, expected_job, result_job):
+        """
+        Compare result job with expected job
+        :param expected_job: job dict from extepected jobs
+        :param result_job: job item from spider crawl
+        :return:
+        """
         result_job_as_dict = result_job.to_dict()
         for field_name in expected_job:
             if field_name not in result_job_as_dict:
