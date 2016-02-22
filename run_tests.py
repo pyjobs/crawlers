3fcd07a31765d7c5e0ac4db9c750e2f1347f0b23
Bastien Sevajol <bastien.sevajol@algoo.fr>
2016-02-22 10:35:36 +0100

    multiple tests in run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -1,10 +1,24 @@
 # -*- coding: utf-8 -*-
 import unittest
 
-from tests.functional.spiders.afpy import TestAfpySpider
+test_modules = [
+    'tests.functional.spiders.afpy.TestAfpySpider',
+    'tests.functional.checks.TestChecks',
+    ]
 
-suite = unittest.TestLoader().loadTestsFromTestCase(TestAfpySpider)
-test_result = unittest.TextTestRunner(verbosity=2).run(suite)
+suite = unittest.TestSuite()
+
+for t in test_modules:
+    try:
+        # If the module defines a suite() function, call it to get the suite.
+        mod = __import__(t, globals(), locals(), ['suite'])
+        suitefn = getattr(mod, 'suite')
+        suite.addTest(suitefn())
+    except (ImportError, AttributeError):
+        # else, just load all the test cases from the module.
+        suite.addTest(unittest.defaultTestLoader.loadTestsFromName(t))
+
+test_result = unittest.TextTestRunner().run(suite)
 
 if test_result.failures or test_result.errors:
     exit(1)
